489cec977af608e5f2cd3909d2d06058d4d0702c
Kristoffer rye <kristofferzrye@hotmail.com>
2022-04-23 21:37:24 +0200

    modal and bug fixes

--- components/Header.vue
+++ components/Header.vue
@@ -1,12 +1,39 @@
 <template>
-<header class="flex justify-between items-center h-32 p-8 bg-slate-800 text-white">
-    <NuxtLink to="/">
-        <span>LOGO</span>
-    </NuxtLink>
-    <span>Antall: {{ cart.length }}</span>
+<header class="flex justify-between items-center h-24 bg-black text-white border border-green-400">
+    <Modal :visible="showModal" @close="toggleShowModal" :title="'Add new product'" :description="'Fill in the form to add the new product'">
+        <Form @added="refresh" @close="toggleShowModal"/>
+    </Modal>
+    <div class="flex items-center h-full">
+        <NuxtLink to="/">
+            <span><img src="~/assets/images/monster_logo.jpg" class="w-24 h-12"/></span>
+        </NuxtLink>
+    </div>
+    <div class="flex justify-between space-x-10 mr-4 ">
+        <button @click="toggleShowModal">
+            <div class="flex  hover:bg-green-500 px-4 items-center rounded-md">
+                <img src="~/assets/svg/plus.svg" class="w-10"/>
+                <div class="flex justify-center items-center ">
+                    <p class="text-white">Add</p>
+                </div>
+            </div>
+        </button>
+        <NuxtLink to="/" class="hover:bg-green-500 rounded-md">
+            <div class="flex justify-between space-x-2 mr-6">
+                <img src="~/assets/svg/shopping-cart.svg" class="w-10 "/>
+                <div class="flex justify-center items-center ">
+                    <p class="text-white">Antall: {{ cart.length }}</p>
+                </div>
+            </div>
+        </NuxtLink>
+    </div>
 </header>
 </template>
 
 <script setup>
+const showModal = ref(true)
+const toggleShowModal = () => showModal.value = !showModal.value
+
 const { cart } = useCart()
+// const emit = defineEmits(["open-add-modal"])
+// const openAddModal = () => emit("open-add-modal")
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <script setup>
 const { data: products, refresh } = await useFetch("/api/products")
-
 useHead({
     title: "Startsiden",
     meta: [{ name: "description", content: "my site"}]
@@ -8,11 +7,11 @@ useHead({
 </script>
 
 <template>
-<main>
-    <div class="grid grid-cols-4 gap-4">
-        <Card v-for="(product, index) in products" :key="index" :product="product" @deleted="refresh"/>
-    </div>
-    <Form @added="refresh"/>
-</main>
+    <main>
+        <div class="grid grid-cols-4 gap-4">
+            <Card v-for="(product, index) in products" :key="index" :product="product" @deleted="refresh"/>
+        </div>
+        <Form @added="refresh"/>
+    </main>
 </template>
 
