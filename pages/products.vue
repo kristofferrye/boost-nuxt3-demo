fe12788f4299719111937e7da3146be4ffdffd58
Kristoffer rye <kristofferzrye@hotmail.com>
2022-04-25 16:11:00 +0200

    emoji

--- pages/products.vue
+++ pages/products.vue
@@ -13,6 +13,10 @@
     const filteredProducts = computed(() => {
         return products.value.filter(p => p.sugarfree === filter.value.sugarfree)
     })
+
+    const allergies = [...new Set(products.value.map(p => p.allergies).flat())]
+
+    const logToConsole = (msg) => console.log(msg)
 </script>
 
 <template>
@@ -20,6 +24,12 @@
         <Form @added="refresh" @close="toggleShowModal"/>
     </Modal>
     Sugarfree<input type="checkbox" v-model="filter.sugarfree">
+    <div class="flex flex-wrap justify-between px-6">
+        <div class="flex items-center" v-for="(allergy, index) in allergies" :key="index">
+            <label for="allergy">😋{{ allergy }}: </label>
+            <input class="ml-2" type="checkbox" v-model="filter[allergy]" @change="logToConsole(allergy)">
+        </div>
+    </div>
     <main class="mb-16">
         <div class="flex justify-center items-center py-4">
             <h1 class="text-4xl tracking-tight sm:text-5xl md:text-6xl text-lime-500 font-thin pb-4 mr-6">Monster Store</h1>
