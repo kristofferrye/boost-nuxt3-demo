23f17085184794324451a4509b8299f12089ee5e
Kristoffer rye <kristofferzrye@hotmail.com>
2022-04-28 11:35:04 +0200

    testing

--- components/Header.vue
+++ components/Header.vue
@@ -17,7 +17,7 @@
 <header class="flex justify-between items-center h-24 bg-black text-white">
     <div class="flex items-center h-full">
         <!-- <NuxtLink to="/"><span><img src="~/assets/images/monster_logo.jpg" class="w-24 h-12"/></span></NuxtLink> -->
-        <a href="/"><span><img src="~/assets/images/monster_logo.jpg" class="w-24 h-12"/></span></a>
+        <a href="/"><span><img src="~/assets/images/monster_logo.jpg" class="w-24 h-12 "/></span></a>
     </div>
     <NuxtLink to="/cart" class="hover:bg-lime-500 rounded-md mr-8 p-4">
         <div class="flex justify-between space-x-2 mr-6">
