96192c2648a38cff4a205fe28c869a3cf33618e4
Kristoffer rye <kristofferzrye@hotmail.com>
2022-04-28 13:11:36 +0200

    port number 8080

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss', '@formkit/nuxt']
+    modules: ['@nuxtjs/tailwindcss', '@formkit/nuxt'],
+    server: {
+        port: 8080
+    }
 })
