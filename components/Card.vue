cab141af269b8aeaf0225099747e49d4dd27efeb
Kristoffer rye <kristofferzrye@hotmail.com>
2022-04-29 11:33:30 +0200

    fix delete

--- components/Card.vue
+++ components/Card.vue
@@ -11,7 +11,7 @@ const addToList = () => { addToCart(props.product) }
 const deleteProduct = async () => {
     try {
         // await useFetch("/api/products", { method: "delete", body: props.product.id })
-        await useFetch(`https://monster-store.herokuapp.com/food-items${props.product.id}`, { method: "delete"})
+        await useFetch(`https://monster-store.herokuapp.com/food-items/${props.product.id}`, { method: "delete"})
         emit("deleted")
         console.log(`Deleting product with id: ${props.product.id}`)
     } catch (error) {
